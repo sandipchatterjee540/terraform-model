9e14d3d65225e407ac021a71a3496b8ba27823ac
sandipchatterjee540 <sandipchatterjee540@gmail.com>
2020-09-03 15:51:50 +0530

    add instance

---
+++ instance.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "test" {
+    ami = "ami-01c36f3329957b16a"
+    instance_type = "t2.micro"
+}
+
+output "public-ip" {
+    value = "${aws_instance.test.public_ip}"
+}
+
+output "private-ip" {
+    value = "${aws_instance.test.private_ip}"
+}
+
+output "private-ip" {
+    value = "${aws_instance.test.private_ip}"
+}
+
